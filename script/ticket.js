418aa0588efd91437e9bd6149fff72e21a617918
ataharhossain <smataharhossain.20cse038.bd@gmail.com>
2024-02-22 15:04:48 +0600

    second commit

--- script/ticket.js
+++ script/ticket.js
@@ -1,7 +1,59 @@
+let totalSeat = 40;
+let selectedSeat = 0;
+let totalPrice = 0;
+let availableSeat = 4;
+let discountPrice = 0;
 let seatDiv= document.getElementById('classKBD');
-let seats = seatDiv.document.getElementsByClassName('kbd');
-let count=0;
-for( let i=0; i<seats.length; i++){
-    count+1;
+const seats = document.querySelectorAll('.seatlist');
+for(let i = 0; i < seats.length; i++) {
+    const seat = seats[i];
+    seat.addEventListener('click', function(event){
+        if(availableSeat <= 0){
+            alert("You Can't Buy all The seat IDIOT !!");
+            return;
+        }
+    const update = document.getElementById('push-seat-details');
+    const mainDiv = document.createElement("div");
+    const seatDiv = document.createElement("div");
+    const classDiv = document.createElement("div");
+    const priceDiv = document.createElement("div");
+
+    seatDiv.innerText = seat.innerText;
+    classDiv.innerText = "Economy";
+    priceDiv.innerText = "550";
+    mainDiv.appendChild(seatDiv);
+    mainDiv.appendChild(classDiv);
+    mainDiv.appendChild(priceDiv);
+        mainDiv.classList.add("flex");
+        mainDiv.classList.add("justify-between");
+    update.appendChild(mainDiv);
+    setValue('total-seat', --totalSeat);
+    setValue('selected-seat', ++selectedSeat);
+    setBackGround(seat.innerText);
+    totalPrice = totalPrice + 550;
+    setValue("total-price", totalPrice);
+    setValue("grand-total", totalPrice);
+
+        availableSeat--;
+    })
 }
-console.log(count);
+function validCoupon() {
+    const val1 = document.getElementById('input-field');
+    const val =val1.value;
+    if(val === "NEW15" || val === "Couple 20"){
+        if(val === "NEW15") {
+            discountPrice = discountPrice * 0.15;
+        }
+        else {   
+      discountPrice = discountPrice * 0.20;
+    }
+    }
+    else {
+        alert("Wrong Coupon");
+        document.getElementById("input-field").value= " ";
+    }
+    let grandValue = totalPrice - discountPrice;
+    setValue("grand-total", grandValue);
+
+    console.log("connected");
+}
--- script/utility.js
+++ script/utility.js
@@ -1,6 +1,18 @@
-// function aboutSeat(btn) {
-//   const button =document.getElementById(btn.id).style.backgroundColor ='green';
-//   if(button === 'green'){
-//     console.log("green");
-//   }
+function getValue (ID) {
+    const value = document.getElementById(ID);
+    const valueText = value.innerText;
+    const valueNumber = parseInt(valueText);
+    return valueNumber;
+}
+function setValue(ID, val) {
+    const value = document.getElementById(ID);
+    value.innerText = val;
+}
+function setBackGround(ID) {
+    document.getElementById(ID).classList.add("addBg")
+}
+
+// function getValue (ID) {
+//     const text = document.getElementById(ID).innerText;
+//     return text;
 // }
