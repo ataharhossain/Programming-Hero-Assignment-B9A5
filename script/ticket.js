fd8d99db03794ae1c5ebecca6c4eff0aa94374bf
ataharhossain <smataharhossain.20cse038.bd@gmail.com>
2024-02-22 13:44:55 +0600

    first commit

---
+++ script/ticket.js
@@ -0,0 +1,7 @@
+let seatDiv= document.getElementById('classKBD');
+let seats = seatDiv.document.getElementsByClassName('kbd');
+let count=0;
+for( let i=0; i<seats.length; i++){
+    count+1;
+}
+console.log(count);
---
+++ script/utility.js
@@ -0,0 +1,6 @@
+// function aboutSeat(btn) {
+//   const button =document.getElementById(btn.id).style.backgroundColor ='green';
+//   if(button === 'green'){
+//     console.log("green");
+//   }
+// }
